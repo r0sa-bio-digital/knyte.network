3516457a25573be6e2651ed0d79382d150f74e59
x-rodionov <59539386+x-rodionov@users.noreply.github.com>
2021-02-27 01:02:10 +0600

    env done right

--- app.ts
+++ app.ts
@@ -1,6 +1,5 @@
 import { serve } from "https://deno.land/std/http/server.ts";
 import { serveFile } from "https://deno.land/std/http/file_server.ts";
-import { config } from "https://deno.land/x/dotenv/mod.ts";
 import * as flags from "https://deno.land/std/flags/mod.ts";
 
 const DEFAULT_PORT = 8080;
@@ -8,7 +7,6 @@ const argPort = flags.parse(Deno.args).port;
 const port = argPort ? Number(argPort) : DEFAULT_PORT;
 
 const server = serve({ port: port });
-const env = config();
 console.log("http://localhost:" + port);
 
 async function fileExists(path: string) {
@@ -34,12 +32,8 @@ for await (const req of server) {
   }
   else if (req.url === "/commit")
   {
-    console.log(env);
-    console.log(env.GITHAB_PAT);
-    console.log(env.TEST);
-    console.log(Deno.env.get("GITHAB_PAT"));
-    console.log(Deno.env.get("TEST"));
-    req.respond({ body: env.GITHAB_PAT });
+    const pat = Deno.env.get("GITHAB_PAT"));
+    req.respond({ body: Deno.env.get("TEST") });
   }
   else if (req.url === "/ping")
     req.respond({ body: `Hello World! Deno ${Deno.version.deno} is in charge.\n` });

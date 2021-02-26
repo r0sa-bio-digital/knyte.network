aad9bec1876256852ef15c35271c6ba8a370c39a
x-rodionov <59539386+x-rodionov@users.noreply.github.com>
2021-02-26 22:55:58 +0600

    static server code added

--- app.ts
+++ app.ts
@@ -1,13 +1,39 @@
 import { serve } from "https://deno.land/std/http/server.ts";
+import { serveFile } from "https://deno.land/std@0.58.0/http/file_server.ts";
 import * as flags from "https://deno.land/std/flags/mod.ts";
 
 const DEFAULT_PORT = 8080;
 const argPort = flags.parse(Deno.args).port;
 const port = argPort ? Number(argPort) : DEFAULT_PORT;
 
-const s = serve({ port: port });
+const server = serve({ port: port });
 console.log("http://localhost:" + port);
 
-for await (const req of s) {
-  req.respond({ body: "Hello World from Deno\n" });
+async function fileExists(path: string) {
+  try {
+    const stats = await Deno.lstat(path);
+    return stats && stats.isFile;
+  } catch(e) {
+    if (e && e instanceof Deno.errors.NotFound) {
+      return false;
+    } else {
+      throw e;
+    }
+  }
+}
+
+for await (const req of server) {
+  const path = `${Deno.cwd()}/public${req.url}`;
+  if (await fileExists(path)) {
+    const content = await serveFile(req, path);
+    req.respond(content);
+    continue;
+  }
+
+  if (req.url === '/')
+    req.respond({ body: "Hello World from Deno Home\n" });
+  else if (req.url === '/about')
+    req.respond({ body: "About" });
+  else
+    req.respond({status: 404});
 }
